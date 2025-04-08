497e6545a58cdcac914d23d8a4536125552e9b28
lucas-ohlin <lucas.persson.ohlin@hotmail.com>
2025-04-08 15:26:31 +0200

    Updated the name to better reflect what it's doing, also additional methods to notify user when joining / leaving

---
+++ src/server.js
@@ -0,0 +1,58 @@
+const io = require('socket.io')(3000, {cors: { origin: '*' }});
+const roomMembers = {};
+
+io.on('connection', socket => {
+  socket.on('join', room => {
+    socket.join(room);
+    socket.room = room;
+
+    const userId = `User-${socket.id.slice(-4)}`;
+    socket.userId = userId;
+
+    if (!roomMembers[room]) roomMembers[room] = [];
+    roomMembers[room].push(userId);
+
+    const roomSize = roomMembers[room].length;
+    socket.emit('initiator', roomSize === 1);
+
+    // Notify all users in the room
+    io.to(room).emit('userJoined', { user: userId, message: `${userId} joined the room.` });
+    io.to(room).emit('participants', roomMembers[room]);
+  });
+
+  socket.on('signal', ({ room, data }) => {
+    socket.to(room).emit('signal', data);
+  });
+
+  socket.on('disconnect', () => {
+    const room = socket.room;
+    const userId = socket.userId;
+
+    if (room && roomMembers[room]) {
+      roomMembers[room] = roomMembers[room].filter(u => u !== userId);
+
+      socket.to(room).emit('userLeft', { user: userId, message: `${userId} left the room.` });
+      socket.to(room).emit('participants', roomMembers[room]);
+
+      if (roomMembers[room].length === 0) {
+        delete roomMembers[room];  
+      }
+    }
+  });
+
+  socket.on('leave', () => {
+    const room = socket.room;
+    const userId = socket.userId;
+  
+    if (room && roomMembers[room]) {
+      roomMembers[room] = roomMembers[room].filter(u => u !== userId);
+  
+      io.to(room).emit('userLeft', { user: userId, message: `${userId} left the room.` });
+      io.to(room).emit('participants', roomMembers[room]);
+  
+      if (roomMembers[room].length === 0) {
+        delete roomMembers[room];
+      }
+    }
+  });  
+});
